e536a391206e845fd2e5dcce0710a677e09cec53
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-09-05 11:44:32 +0300

    create map section

---
+++ src/components/MapSection/MapSection.tsx
@@ -0,0 +1,15 @@
+
+const MapSection = () => {
+  return (
+    <div className="bg-amber w-screen">
+      <div className="max-w-[1440px] background-map mx-auto h-[717px] laptop:px-20 py-10 flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-3 text-foreground-light-primary font-bold">
+          <div className="text-6xl tablet:text-8xl ">11,658,467</div>
+          <div className="text-4xl tablet:text-[56px] text-center">Shoes Collected</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MapSection
